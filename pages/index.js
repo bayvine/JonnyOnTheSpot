713792fe88ae0baf2326d4ae5dc912d3099b37e0
Brainilio <brainilioir@gmail.com>
2022-12-28 15:22:09 -0800

    TEST faqschema

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,21 @@ const Page = ({ page, navigation, settings }) => {
 		openingHours: localBusinessFromPrismic.openinghours,
 		address: localBusinessFromPrismic.address,
 	}
+
+	const faqSchema = {
+		"@context": "https://schema.org",
+		"@type": "FAQPage",
+		mainEntity: [
+			{
+				"@type": "Question",
+				name: "What is the return policy?",
+				acceptedAnswer: {
+					"@type": "Answer",
+					text: "<p>Most unopened items in new condition and returned within <b>90 days</b> will receive a refund or exchange. Some items have a modified return policy noted on the receipt or packing slip. Items that are opened or damaged or do not have a receipt may be denied a refund or exchange. Items purchased online or in-store may be returned to any store.</p><p>Online purchases may be returned via a major parcel carrier. <a href=https://example.com/returns> Click here </a> to initiate a return.</p>",
+				},
+			},
+		],
+	}
 	return (
 		<div className="overflow-x-hidden">
 			<Head>
@@ -73,6 +88,13 @@ const Page = ({ page, navigation, settings }) => {
 					type="application/ld+json"
 					dangerouslySetInnerHTML={{
 						__html: JSON.stringify(localBusinessSchema),
+					}}
+				></script>
+
+				<script
+					type="application/ld+json"
+					dangerouslySetInnerHTML={{
+						__html: JSON.stringify(faqSchema),
 					}}
 				></script>
 			</Head>
